68711ca3ec03b367151c3b54376b3263a584c1ff
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-09 11:14:18 -0400

    Inicio de Aplicacion

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './assets/AESocial.jpg'
+
+function App() {
+   return (
+      <>
+         {/* Header */}
+         <img src={logo} alt="Logo AE Social" />
+         <main>
+            {/* Formulario para Tweets */}
+            <section>
+               {/* Tweets */}
+            </section>
+         </main>
+         {/* Footer */}
+      </>
+   )
+}
+
+export default App
