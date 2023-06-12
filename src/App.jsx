dd50b192a2e8b888d6575341aa1156718e73e334
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-12 13:13:24 -0400

    App Actualizada

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,35 @@ const tweetsArray = [
    {
       id: '7643756347865',
       user: 'Avendaosander',
-      tweet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam no.'
+      tweet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam no.',
+      like: false,
+      fav: false
    },
    {
       id: '843278573432847',
       user: 'FrontII',
-      tweet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus en nisl neque. Proin in turpis vel enim interdum bibendum. Etiam.'
+      tweet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus en nisl neque. Proin in turpis vel enim interdum bibendum. Etiam.',
+      like: false,
+      fav: false
    },
    {
       id: '463624576235',
       user: 'Alejo2608',
-      tweet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin fermentum risus, ut posuere turpis congue at. Duis maximus nisi et lectus hendrerit, euismod ultrices turpis aliquet. Aenean eu feugiat.'
+      tweet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin fermentum risus, ut posuere turpis congue at. Duis maximus nisi et lectus hendrerit, euismod ultrices turpis aliquet. Aenean eu feugiat.',
+      like: false,
+      fav: false
    }
 ]
 
 function App() {
-   // const [tweets, setTweets] = useState(tweetsArray)
+   const [tweets, setTweets] = useState(tweetsArray)
    const [modalDelete, setModalDelete] = useState(false)
+   const [tweetDelete, setTweetDelete] = useState('')
+   
+   // localStorage.setItem('Tweet', JSON.stringify(tweets))
 
-   const handleModalDelete = () => {
+   const handleModalDelete = (tweetID = null) => {
+      tweetID && setTweetDelete(tweetID)
       setModalDelete(!modalDelete)
    }
 
@@ -37,12 +47,17 @@ function App() {
          <main>
             {/* Formulario para Tweets */}
             <section className='flex flex-col justify-center items-center gap-5 mb-40 sm:mb-20 '>
-               {tweetsArray.map((tweet) => (
-                  <Tweets key={tweet.id} handleModalDelete={handleModalDelete} tweet={tweet}/>
-               ))}
+               {tweets.length > 0 
+                  ? (
+                     tweets.map((tweet) => (
+                        <Tweets key={tweet.id} handleModalDelete={handleModalDelete} tweet={tweet} setTweets={setTweets}/>
+                     ))
+                  ) : (
+                     <h2 className='text-slate-300 text-lg'>No hay Tweets disponibles</h2>
+                  )}
             </section>
          </main>
-         {modalDelete && <ModalDelete handleModalDelete={handleModalDelete}/>}
+         {modalDelete && <ModalDelete handleModalDelete={handleModalDelete} tweetID={tweetDelete} setTweets={setTweets}/>}
          <Footer/>
       </>
    )
