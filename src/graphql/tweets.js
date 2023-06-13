bf63dec9abec45f048da9b5a254b24dcb2830223
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-13 17:10:59 -0400

    Consultas de GraphQL

---
+++ src/graphql/tweets.js
@@ -0,0 +1,46 @@
+import { gql } from "@apollo/client";
+
+export const GET_TWEETS = gql`
+   {
+      tweets {
+         _id,
+         username,
+         message,
+         like,
+         fav
+      }
+   }
+`
+export const CREATE_TWEET = gql`
+   mutation($username: String, $message: String) {
+      createTweet(username: $username, message: $message) {
+         _id,
+         username,
+         message,
+         like,
+         fav
+      }
+   }
+`
+export const UPDATE_TWEET = gql`
+   mutation($id: ID!, $like: Boolean, $fav: Boolean) {
+      updateTweet(_id: $id, like: $like, fav: $fav){
+         _id,
+         username,
+         message,
+         like,
+         fav
+      }
+   }
+`
+export const DELETE_TWEET = gql`
+   mutation($id: ID!) {
+      deleteTweet (_id: $id){
+         _id,
+         username,
+         message,
+         like,
+         fav
+      }
+   }
+`
